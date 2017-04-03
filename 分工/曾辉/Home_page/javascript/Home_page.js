946f607081b3b11d5056de56e58dc680270f7800
HZ <hzeng1998@gmail.com>
2017-04-03 23:30:57 +0800

    test ajax

--- 分工/曾辉/Home_page/javascript/Home_page.js
+++ 分工/曾辉/Home_page/javascript/Home_page.js
@@ -11,7 +11,26 @@ window.onload = function () {
     target_2.onmouseout= function () {
         document.getElementById("nav-platform-menu").style.display = "none";
     };
-
-
-
 };
+
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: './data/data.json',
+        success: function (data) {
+            var article =  $("#home-blog .blog-article");
+            var title = $("#home-blog .blog-title");
+            for (var j = 0; j <  title.length; j++) {
+                title[j].innerHTML = data["title"];
+            }
+            for (var i = 0; i <  article.length; i++) {
+                article[i].innerHTML = data["article"][i];
+            }
+        },error:function(XMLHttpRequest, textStatus, errorThrown){
+            console.log(XMLHttpRequest.status);
+            console.log(XMLHttpRequest.readyState);
+            console.log(textStatus);
+        }
+    });
+});
