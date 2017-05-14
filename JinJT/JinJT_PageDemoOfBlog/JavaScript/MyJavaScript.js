ea3d0fb23ebcd688ebf910d1529dde1d9f9b6f7f
unknown <>
2017-05-14 19:59:49 +0800

    dropdown_menu And top_botton

---
+++ JinJT/JinJT_PageDemoOfBlog/JavaScript/MyJavaScript.js
@@ -0,0 +1,140 @@
+window.onload=function()
+{
+  var passage_lists=document.getElementById("JJT_passage_lists");
+    var passage_time_lists={
+        "2016":{
+            "January":1,
+            "February":2,
+            "March":3,
+            "April":4,
+            "May":5,
+            "June":6,
+            "July":7,
+            "August":8,
+            "September":9,
+            "October":10,
+            "November":11,
+            "December":12
+        },
+        "2017":{
+            "text1":1,
+            "text2":2
+        }
+    };
+
+  for (var i in passage_time_lists)
+  {
+      var time_list=document.createElement("ul");
+      time_list.addEventListener("click",function(e)
+      {
+
+      });
+      time_list.className="JJT_dropdown";
+      time_list.innerHTML=i;
+      for (var j in passage_time_lists[i])
+      {
+
+          var item=document.createElement("li");
+          item.innerHTML=j;
+          item.className="JJT_dropdown_item";
+          item.id="JJT_dropdown_item";
+          time_list.appendChild(item);
+      }
+      passage_lists.appendChild(time_list);
+  }
+
+    passage_lists.addEventListener("click",function(e)
+    {
+        if (e.target.nodeName=='UL')
+        {
+            var items=e.target.childNodes;
+            for (var i=0;i<items.length;++i)
+            {
+                if (items[i].nodeName.toUpperCase()=="LI")
+                {
+                    if (items[i].style.display=="none")
+                    {
+                        items[i].style.display="block";
+                    }
+                    else
+                    {
+                        items[i].style.display="none";
+                    }
+                }
+            }
+        }
+        else
+        {
+            (function()
+            {
+                var request=null;
+                if (window.XMLHttpRequest)
+                {
+                    request=new XMLHttpRequest();
+                }
+                else
+                {
+                    if (window.ActiveXObject)
+                    {
+                        request= new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+                }
+                if (request)
+                {
+                    console.log("123");
+                    var display_txt_name=e.target.innerHTML;
+                    console.log(request.responseText);
+                    request.open("GET","hello.txt",true);
+                    request.onreadystatechange=function()
+                    {
+                        if (request.readyState===4)
+                        {
+                            if (request.status==200||request==0)
+                            {
+                                document.getElementById("JJT_passage").innerHTML=request.responseText;
+                            }
+                        }
+                    };
+                    request.send=null;
+                }
+                else
+                {
+                    alert("error0");
+                }
+            }());
+        }
+    });
+
+
+
+
+    var Botton_To_Top=document.getElementsByClassName("JJT_Botton_To_Top")[0];
+    Botton_To_Top.onclick=function()
+    {
+      var sth;
+        sth=setInterval(scrollTop,20);
+        function scrollTop()
+        {
+                if (document.body.scrollTop<=0)
+                {
+                    clearInterval(sth);
+                }
+                else
+                {
+                    window.scrollBy(0,-60);
+                }
+        }
+    };
+
+};
+
+
+$(document).ready(function()
+{
+   $(".JJT_dropdown_item").click(function()
+   {
+       console.log("123123");
+       var htmlobj=$.ajax({url:"hello.txt",async:false});
+       $(".passagedetail").html(htmlobj.responseText);
+   })
+});
